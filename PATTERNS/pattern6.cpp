ab9114ced925f82d5b635b5bf25a47ea8c255b87
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2023-07-27 16:40:47 +0530

    PATTERNS ADDED

---
+++ PATTERNS/pattern6.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n ;
+    cout << "ENTER THE NUMBER : ";
+    cin>>n;
+
+    
+    for(int i = 1 ; i <=n; i++){
+        for (int j = 1; j <= n-i+1; j++)
+        {
+            cout << " " << j << " ";
+        }
+        cout << endl;
+
+    }
+
+}
