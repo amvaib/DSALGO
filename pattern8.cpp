4864abceffe1a178204f6ba70b025877cf182be6
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2023-07-27 17:30:20 +0530

    PATTERNS UPDATED

---
+++ pattern8.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n ;
+    cout << "ENTER THE NUMBER : ";
+    cin>>n;
+
+    
+    for(int i = 1 ; i <=n; i++){
+
+        //space
+        for (int j = 1; j <= i-1; j++)
+        {
+            cout << " ";
+        }
+        
+
+        //stars
+
+        for (int j = 1; j <= 2*n-(2*i-1); j++)
+        {
+            cout << "*";
+        }
+        
+
+        //space
+
+        for (int j = 1; j <= i-1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+
+    }
+
+}
