f030dc10340ee6b8e13052cc5b630993e520e410
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2023-07-27 16:08:07 +0530

    pattern 1 added

---
+++ BASICCPP/basic.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    // int a = 2;
+    // int b = a + 1;
+
+    // if((a=3) == b ){
+    //     cout << "FUCK YOU"<< endl;
+
+    // }
+    // else{
+    //     cout << "I LOVE YOU" << endl;
+    
+    // }
+    // }
+
+    
+
