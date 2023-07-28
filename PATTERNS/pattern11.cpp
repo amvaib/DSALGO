b8257e68c145589fe64434a1562f0557d53aea7a
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2023-07-28 17:41:20 +0530

    patterns added

---
+++ PATTERNS/pattern11.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cout << "ENTER A NUMBER : ";
+    cin >> n;
+
+    int start;
+
+    for(int i = 1; i<=n; i++){
+        if(i%2==0){
+            start = 0;
+        }
+        else{
+            start = 1;
+
+        }
+
+        for(int j = 1; j<=i ; j++){
+            cout << start;
+            start = 1  - start;
+        }
+        cout << endl;
+    }
+}
---
+++ PATTERNS/pattern12.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n ;
+    cout << "ENTER THE NUMBER : ";
+    cin>>n;
+
+    
+    for(int i = 1 ; i <=n; i++){
+
+        //counting
+        for (int j = 1; j <=i; j++)
+        {
+            cout << j;
+        }
+
+        for (int j = 1; j <=2*(n-i); j++)
+        {
+            cout << " ";
+        }
+
+        
+
+        for(int j = i; j>=1; j--){
+            cout<< j;
+        }
+
+        cout << endl;
+    }
+
+}
