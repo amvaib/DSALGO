dc63e307261b5bc4c846bb2ab9ee675774e5f85f
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2023-09-02 16:18:08 +0530

    LEETCODE ADDED

---
+++ QUESTIONS/leetcodeq1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n ;
+    cout << "ENTER A THREE DIGIT NUMBER : ";
+    cin>>n;
+
+    int sum = 0;
+    int product = 1;
+
+    while(n!=0){
+        int digit = n%10;
+        product = product * digit;
+        sum = sum + digit;
+
+        n = n/10;
+    }
+
+    int answer = product-sum;
+    return answer;
+}
