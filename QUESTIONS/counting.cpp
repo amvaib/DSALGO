361cf0ec9a138f404d39ff42436915599ced0255
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2023-09-05 00:41:12 +0530

    COUNTING

---
+++ QUESTIONS/counting.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void count(int n){
+    for(int i = 1; i<=n; i++){
+        cout << i << endl;
+    }
+}
+
+int main(){
+    int n ;
+    cout << "ENTER A NUMBER : ";
+    cin>>n;
+    
+    count(n);
+
+}
